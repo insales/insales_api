e5ea0642f8b581508b9acc243adfb13148efb7bf
Dmitriy Gusev <dg@insales.ru>
2012-05-18 16:27:08 +0400

    now can update order_lines through

--- lib/insales_api/resources/order.rb
+++ lib/insales_api/resources/order.rb
@@ -1,3 +1,15 @@
 module InsalesApi
-  class Order < Base; end
+  class Order < Base
+    def order_lines_attributes
+      @order_lines_attributes = []
+      order_lines.each do |order_line|
+        @order_lines_attributes << order_line.as_json['order_line']
+      end
+      @order_lines_attributes
+    end
+
+    def to_xml(options = {})
+      super(options.merge({:methods => :order_lines_attributes}))
+    end
+  end
 end
