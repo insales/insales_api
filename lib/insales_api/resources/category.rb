7abb19247e156a45336c3a1133c93716c4e3c13d
Dmitriy Gusev <dg@insales.ru>
2012-07-10 12:35:09 +0400

    add Category.set_products_category method

--- lib/insales_api/resources/category.rb
+++ lib/insales_api/resources/category.rb
@@ -1,3 +1,7 @@
 module InsalesApi
-  class Category < Base; end
+  class Category < Base
+    def self.set_products_category new_category_id, products_ids
+      connection.put('/admin/categories/set_products_category.xml', {:id => new_category_id, :product_ids => Array(products_ids)}.to_xml(:root => 'new_category'), headers)
+    end
+  end
 end
--- lib/insales_api/version.rb
+++ lib/insales_api/version.rb
@@ -1,3 +1,3 @@
 module InsalesApi
-  VERSION = "0.0.4"
+  VERSION = "0.0.5"
 end
