70817d6953ee9824cffd13969a4bf24cfcb56079
Vladimir Tsukanov <tsukanov.vl@gmail.com>
2013-05-21 18:50:35 +0400

    Добавил групповое обновление модификаций

--- lib/insales_api/resources/variant.rb
+++ lib/insales_api/resources/variant.rb
@@ -1,5 +1,11 @@
 module InsalesApi
   class Variant < Base
     self.prefix = "/admin/products/:product_id/"
+
+    # variants_attrs - массив c модицикациями в формате [{:id => 1, :price => 340, :quantity => 4}, {:id => 2, :price => 350, :quantity => 5}]
+    def self.group_update variants_attrs
+      connection.put("/admin/products/variants_group_update.xml", { :variants => variants_attrs }.to_xml, headers)
+    end
+
   end
 end
