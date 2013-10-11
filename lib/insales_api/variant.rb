87daa7acffdc2a4cda68c9c192a3fe1c941eac0a
Max Melentiev <melentievm@gmail.com>
2013-10-11 20:12:10 +0400

    Починил Variant::group_update

--- lib/insales_api/variant.rb
+++ lib/insales_api/variant.rb
@@ -1,11 +1,27 @@
 module InsalesApi
   class Variant < Base
-    self.prefix = "/admin/products/:product_id/"
+    GROUP_UPDATE_PATH = '/admin/products/variants_group_update.xml'
+    self.prefix = '/admin/products/:product_id/'
 
-    # variants_attrs - массив c модицикациями в формате [{:id => 1, :price => 340, :quantity => 4}, {:id => 2, :price => 350, :quantity => 5}]
-    def self.group_update variants_attrs
-      connection.put("/admin/products/variants_group_update.xml", { :variants => variants_attrs }.to_xml, headers)
+    class << self
+      # Выполняет обновление всех переданных модификаций.
+      # variants - массив c модицикациями в формате
+      #
+      #   [
+      #     {
+      #       id: 1,
+      #       price: 340,
+      #       quantity: 4,
+      #     },
+      #     {
+      #       id: 2,
+      #       price: 350,
+      #       quantity: 5,
+      #     },
+      #   ]
+      def group_update(variants)
+        connection.put(GROUP_UPDATE_PATH, variants.to_xml(root: :variants), headers)
+      end
     end
-
   end
 end
