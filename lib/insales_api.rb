2f0c9677c5922788b8a8e2d3e1cb31149789b7d3
Nu-hin <nuinuhin@gmail.com>
2014-06-10 16:37:07 +0400

    wait_retry - Вызов блока с ожиданием, если лимит Insales достигнут

--- lib/insales_api.rb
+++ lib/insales_api.rb
@@ -44,6 +44,48 @@ module InsalesApi
     autoload :Variant
     autoload :Webhook
   end
+
+  class << self
+    # Calls the supplied block. If the block raises <tt>ActiveResource::ServerError</tt> with 503
+    # code which means Insales API request limit is reached, it will wait for the amount of seconds
+    # specified in 'Retry-After' response header. The called block will receive a parameter with
+    # current attempt number.
+    #
+    # ==== Params:
+    #
+    #   +max_attempts+:: maximum number of attempts. Defaults to +nil+ (unlimited).
+    #   +callback+:: +Proc+ or lambda to execute before waiting. Will receive four arguments: number
+    #                of seconds we are going to wait, number of failed attempts, maximum number of
+    #                attempts and the caught <tt>ActiveResource::ServerError</tt>. Defaults to +nil+
+    #                (no callback).
+    #
+    # ==== Example
+    #
+    #   notify_user = Proc.new do |wait_for, attempt, max_attempts, ex|
+    #     puts "API limit reached. Waiting for #{wait_for} seconds. Attempt #{attempt}/#{max_attempts}"
+    #   end
+    #
+    #   InsalesApi.wait_retry(10, notify_user) do |x|
+    #     puts "Attempt ##{x}."
+    #     products = InsalesApi::Products.all
+    #   end
+    def wait_retry(max_attempts = nil, callback = nil, &block)
+      attempts = 0
+
+      begin
+        attempts += 1
+        yield attempts
+      rescue ActiveResource::ServerError => ex
+        raise ex if '503' != ex.response.code.to_s
+        raise ex if max_attempts && attempts >= max_attempts
+        retry_after = (ex.response['Retry-After'] || 150).to_i
+        callback.call(retry_after, attempts, max_attempts, ex) if callback
+        sleep(retry_after)
+        retry
+      end
+    end
+  end
+
 end
 
 require 'insales_api/helpers/init_api'
