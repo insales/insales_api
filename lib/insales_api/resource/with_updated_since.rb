a01506c02663eaff63e9a1306aa426eb7f4e0f86
Vladimir Bedarev <bedarev.vladimir@gmail.com>
2015-07-02 20:06:00 +0300

    Исправляем алгоритм выбора изменившихся товаров.

--- lib/insales_api/resource/with_updated_since.rb
+++ lib/insales_api/resource/with_updated_since.rb
@@ -6,21 +6,56 @@ module InsalesApi
         find_updated_since(updated_since, options, &block)
       end
 
-      def find_updated_since(updated_since, options = {})
-        per_page = options[:per_page] || PER_PAGE_DEFAULT
-        params    = { per_page: per_page }.merge(options[:params] || {})
-        last_id   = nil
-        loop do
-          items = all(params: params.merge(
-            updated_since:  updated_since,
-            from_id:        last_id,
-          ))
-          return unless items.any?
-          yield items
-          return if items.count < per_page
-          last_item     = items.last
-          last_id       = last_item.id
-          updated_since = last_item.updated_at
+      def find_updated_since(updated_since = nil, options = {})
+        limit = options[:per_page] || PER_PAGE_DEFAULT
+        updated_since ||= '2008-01-01'.to_date
+
+        # Сюда мы будем складывать товары с одинаковым updated_at.
+        # Для исключения дубликатов, а они могут быть при перелиствыании,
+        # Используем Hash
+        current_products = {}
+        page = 1
+
+        while true
+          Rails.logger.info "#{updated_since} #{page}"
+          insales_products = InsalesApi::Product.all(
+            params: {
+              with_deleted: 1, updated_since: updated_since, limit: limit, page: page
+            }.merge(options[:params] || {})
+          )
+
+          # Если товары с одинаковым updated_at, не поместились на страницу.
+          # То мы для их выбора используем page. При обновлении товаров с ранее выбранных странц
+          # из-за смещения товаров между страницами мы можем теряем часть товаров.
+          if page > 1 && !current_products.empty? && !insales_products.size.zero? &&
+             current_products[insales_products.first.id]
+
+            page = 1
+            next
+          end
+
+          # На случай если updated_at у всех товаров одинаковый готовимся прочесть следующую cтраницу.
+          page += 1
+
+          insales_products.each do |insales_product|
+            updated_at = insales_product.updated_at.to_datetime
+            if updated_at > updated_since
+              updated_since = updated_at
+
+              yield current_products.values
+
+              # Если updated_since изменился, то нет смысла мотать страницы
+              current_products = {}
+              page = 1
+            end
+
+            current_products[insales_product.id] = insales_product
+          end
+
+          if insales_products.size < limit
+            yield current_products.values
+            break
+          end
         end
       end
     end
