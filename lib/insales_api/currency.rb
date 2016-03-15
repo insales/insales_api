39ffd04352f6fbc255839cfbc5a109e52d13f198
dilcom <dilcom3107@gmail.com>
2016-03-15 16:41:24 +0300

    добавляем прокси для валют

---
+++ lib/insales_api/currency.rb
@@ -0,0 +1,6 @@
+# coding: utf-8
+module InsalesApi
+  class Currency < Base
+    include ActiveResource::Singleton
+  end
+end
---
+++ lib/insales_api/stock_currency.rb
@@ -0,0 +1,5 @@
+# coding: utf-8
+module InsalesApi
+  class StockCurrency < Base
+  end
+end
