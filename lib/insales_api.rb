49b751ddd420f84bb75c754dfa1f66c7baf3d197
Denis Lukyanov <d.lukyanov@secoint.ru>
2017-08-02 17:25:18 +0300

    Add support for CustomStatus resource.

--- lib/insales_api.rb
+++ lib/insales_api.rb
@@ -26,6 +26,7 @@ module InsalesApi
     autoload :Collect
     autoload :Collection
     autoload :Currency
+    autoload :CustomStatus
     autoload :DeliveryVariant
     autoload :DiscountCode
     autoload :Domain
